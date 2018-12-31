ec4b1f0afd13e63392ffadb0b75b8b5833478816
cngeeker365 <277263044@qq.com>
2018-12-31 16:29:30 +0800

    并发版爬虫-每个worker有自己的chan

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -12,20 +12,24 @@ type ConcurrentEngine struct {
 type Scheduler interface {
 	Submit(Request)
 	ConfigMasterWorkerChan(chan Request)
+	WorkerReady(chan Request)
+	Run()
 }
 
 func (e *ConcurrentEngine) Run(seeds ...Request)  {
 	//多个worker共用一个in和out
-	in := make(chan Request)
+	//in := make(chan Request)
 	out := make(chan ParseResult)
-	e.Scheduler.ConfigMasterWorkerChan(in)
+	//e.Scheduler.ConfigMasterWorkerChan(in)
+	e.Scheduler.Run()
 
 	for _,r := range seeds{
 		e.Scheduler.Submit(r)
 	}
 
 	for i:=0;i<e.WorkerCount;i++{
-		createWorker(in, out)
+		//createWorker(in, out)
+		createWorkerQ(out, e.Scheduler)
 	}
 
 	itemCount := 0
@@ -41,9 +45,27 @@ func (e *ConcurrentEngine) Run(seeds ...Request)  {
 	}
 }
 
+func createWorkerQ(out chan ParseResult, s Scheduler) {
+	in := make(chan Request)
+	go func() {
+		for {
+			// tell scheduler i'm ready
+			s.WorkerReady(in)
+
+			request := <- in
+			result, err := worker(request)
+			if err !=nil{
+				continue
+			}
+			out <- result
+		}
+	}()
+}
+
 func createWorker(in chan Request, out chan ParseResult) {
 	go func() {
 		for {
+			// tell scheduler i'm ready
 			request := <- in
 			result, err := worker(request)
 			if err !=nil{
--- crawler/scheduler/queued.go
+++ crawler/scheduler/queued.go
@@ -1,1 +1,50 @@
 package scheduler
+
+import "awesomeProject/crawler/engine"
+
+type QueuedScheduler struct {
+	requestChan chan engine.Request
+	workerChan  chan chan engine.Request
+}
+
+func (s *QueuedScheduler) Submit(r engine.Request) {
+	s.requestChan <- r
+}
+
+func (s *QueuedScheduler) ConfigMasterWorkerChan(chan engine.Request) {
+	panic("implement me")
+}
+
+func (s *QueuedScheduler) WorkerReady(w chan engine.Request) {
+	s.workerChan <- w
+}
+
+func (s *QueuedScheduler) Run() {
+	s.workerChan = make(chan chan engine.Request)
+	s.requestChan = make(chan engine.Request)
+	go func() {
+		var requestQ []engine.Request
+		var workerQ []chan engine.Request
+		for {
+			var activeRequest engine.Request
+			var activeWorker chan engine.Request
+
+			if len(requestQ) > 0 && len(workerQ) > 0 {
+				activeRequest = requestQ[0]
+				activeWorker = workerQ[0]
+			}
+			// r&w 各自独立发生，因此要用select来处理
+			// r 收到，但无法发送给 w，因为不知道w是谁，因此要将收到的 r 放入队列
+			select {
+			case r := <-s.requestChan:
+				requestQ = append(requestQ, r)
+			case w := <-s.workerChan:
+				workerQ = append(workerQ, w)
+			case activeWorker <- activeRequest:
+				workerQ = workerQ[1:]
+				requestQ = requestQ[1:]
+			}
+
+		}
+	}()
+}
