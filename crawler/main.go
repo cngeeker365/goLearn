5ac3ab29a6826b3e24cce558d68a126d5497cd79
cngeeker365 <277263044@qq.com>
2018-12-31 11:31:59 +0800

    并发爬虫

--- crawler/main.go
+++ crawler/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"awesomeProject/crawler/engine"
+	"awesomeProject/crawler/scheduler"
 	"awesomeProject/crawler/zhenai/parser"
 )
 
 func main() {
-	engine.Run(engine.Request{
+	//engine.SimpleEngine{}.Run(engine.Request{
+	//	Url: "http://www.zhenai.com/zhenghun",
+	//	ParserFunc:parser.ParseCityList,
+	//})
+
+	e := engine.ConcurrentEngine{
+		Scheduler: &scheduler.SimpleScheduler{},
+		WorkerCount: 10,
+	}
+	e.Run(engine.Request{
 		Url: "http://www.zhenai.com/zhenghun",
 		ParserFunc:parser.ParseCityList,
 	})
